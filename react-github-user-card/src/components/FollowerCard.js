19e71b96ba8ed84aa45df90866b18096b0b8ebb6
Shafi Masoumi <masoumi2017@icloud.com>
2019-11-05 14:19:13 -0800

    user card ok now

--- react-github-user-card/src/components/FollowerCard.js
+++ react-github-user-card/src/components/FollowerCard.js
@@ -4,6 +4,8 @@ const FollowerCards = props => {
   console.log("card rendered");
   return (
     <div className="followerCard">
+      <img className="followerImage" src={props.image} alt="pic" />
+      <h3>{props.name}</h3>
     </div>
   );
 };
