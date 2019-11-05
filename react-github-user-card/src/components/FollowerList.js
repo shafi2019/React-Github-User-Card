6cf27ddcd5f1fc7df27d50bf662596034e60f1d3
Shafi Masoumi <masoumi2017@icloud.com>
2019-11-05 14:17:30 -0800

    user follower data fetched

--- react-github-user-card/src/components/FollowerList.js
+++ react-github-user-card/src/components/FollowerList.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from "axios";
+import FollowerCard from "./FollowerCard";
 
 class FollowerList extends Component {
   constructor() {
@@ -7,11 +9,36 @@ class FollowerList extends Component {
     console.log("follower list constructed");
   }
 
+  componentDidMount() {
+    console.log(`follower list mounted`);
+    axios
+      .get(`https://api.github.com/users/${this.props.username}/followers`)
+      .then(response => this.setState({ followers: response.data }))
+      .catch(error => console.log("something went wrong"));
+  }
+
+  componentDidUpdate(prevState, prevProps) {
+    if (prevProps !== this.props || prevState !== this.state) {
+      axios
+        .get(`https://api.github.com/users/${this.props.username}/followers`)
+        .then(response => this.setState({ followers: response.data }))
+        .catch(error => console.log("something went wrong"));
+    }
+    console.log(`follower list updated`);
+  }
   render() {
     console.log(`follewer list rendered`);
     return (
       <div className="followersContainer">
-      
+        {this.state.followers
+          ? this.state.followers.map(follower => (
+              <FollowerCard
+                key={follower.login}
+                name={follower.login}
+                image={follower.avatar_url}
+              />
+            ))
+          : null}
       </div>
     );
   }
