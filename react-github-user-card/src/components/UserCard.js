d9a277f2eb1a871d7cf2aa937120faf03c88c4e7
Shafi Masoumi <masoumi2017@icloud.com>
2019-11-05 14:09:45 -0800

    User card and get request to API

--- react-github-user-card/src/components/UserCard.js
+++ react-github-user-card/src/components/UserCard.js
@@ -4,6 +4,14 @@ const UserCard = props => {
   console.log("card rendered");
   return (
     <div className="userCard">
+      <img className="userImage" src={props.image} alt="pic" />
+      <div className="userInfo">
+        <h1 className="userName">{props.name}</h1>
+        <em className="userBio">{props.bio}</em>
+        <p>Location:{props.location}</p>
+        <p>Followers:{props.follower}</p>
+        <p>Following:{props.following}</p>
+      </div>
     </div>
   );
 };
--- react-github-user-card/src/components/UserProfile.js
+++ react-github-user-card/src/components/UserProfile.js
@@ -7,11 +7,20 @@ class UserProfile extends Component {
     console.log(`user profile constructed`);
   }
 
+  componentDidMount() {
+    console.log(`user profile mounted`);
+    axios
+      .get(`https://api.github.com/users/shafi2019`)
+      .then(response => this.setState({ user: response.data }))
+      .catch(error => console.log("something went wrong"));
+  }
+
+
   render() {
     console.log(`user profile rendered`);
     return (
       <div className="appContainer">
-      
+        
       </div>
     );
   }
